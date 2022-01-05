ef47096b0525f13f535e4dba6bb1febb16cabe6c
LucasRodriguesSilva <lucasrodrigues19021998@gmail.com>
2022-01-05 17:07:40 -0300

    Rastreando projeto

---
+++ ConstrutorPublic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConstrutorPublic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
