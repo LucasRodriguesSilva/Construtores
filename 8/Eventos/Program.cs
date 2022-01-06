13948ef1e1092552b2f0176cfd93cc3b84cb5610
LucasRodriguesSilva <lucasrodrigues19021998@gmail.com>
2022-01-06 16:08:18 -0300

    Eventos

---
+++ 8/Eventos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eventos.Models;
+
+namespace Eventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematica m = new Matematica(10,20);
+            m.Somar();
+        }
+    }
+}
